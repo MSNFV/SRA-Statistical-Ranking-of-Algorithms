1c0e45677aebc6564754d3540667623b442ec2bb
Najibeh Farzi-Veijouyeh <90899827+MSNFV@users.noreply.github.com>
2025-10-26 18:27:33 +0330

    Update Example_SRA.m

--- Example_SRA.m
+++ Example_SRA.m
@@ -17,13 +17,21 @@ load('ResultsOA.mat', 'FbestMAT', 'NamesofAlgorithms', 'NamesofFunctions');
 Data = FbestMAT;
 
 % Run SRA method
-[FailureRate, Rank_ID] = SRA(Data);
-
+[P_value h FailureRate, Rank_ID] = SRA(Data);
+  
 % Create results table
 ResultsTable = table(NamesofAlgorithms', FailureRate', Rank_ID', ...
     'VariableNames', {'Algorithm', 'FailureRate', 'Rank_ID'});
-
 disp('=== Example 1 Results ===');
+disp(strcat('P_value=',num2str(P_value(1))));
+if h==0
+     % Reject H0: at least one algorithm performs significantly differently
+    disp('H0 is rejected: significant difference detected between algorithms.');
+else
+    % Fail to reject H0: no significant difference detected
+    disp('H0 is not rejected: no statistically significant difference found.');
+end
+
 disp(ResultsTable);
 
 %% ------------------- Example 2 -------------------
@@ -42,7 +50,7 @@ if abs(sum(Beta) - 1) > 1e-6
 end
 
 % --- Define function groups ---
-% For example: Unimodal (F1–F7), Multimodal (F8–F23), Composite (F24–F29)
+% For example: Unimodal (F1â€“F7), Multimodal (F8â€“F23), Composite (F24â€“F29)
 FunctionGroup = [ones(1,7), 2*ones(1,16), 3*ones(1,6)];
 if numel(FunctionGroup) ~= numel(Functions)
     error('The size of FunctionGroup must match the number of selected Functions.');
@@ -52,11 +60,19 @@ end
 NamesofAlgorithms = NamesofAlgorithms(Algorithms);
 
 % Run SRA method
-[FailureRate, Rank_ID] = SRA(Data, Algorithms, Functions, NamesofAlgorithms, alpha, Beta, FunctionGroup);
+[P_value, h, FailureRate, Rank_ID] = SRA(Data, Algorithms, Functions, NamesofAlgorithms, alpha, Beta, FunctionGroup);
 
 % Create results table
 ResultsTable = table(NamesofAlgorithms', FailureRate', Rank_ID', ...
     'VariableNames', {'Algorithm', 'FailureRate', 'Rank_ID'});
 
 disp('=== Example 2 Results ===');
+disp(strcat('P_value=',num2str(P_value(1))));
+if h==0
+     % Reject H0: at least one algorithm performs significantly differently
+    disp('H0 is rejected: significant difference detected between algorithms.');
+else
+    % Fail to reject H0: no significant difference detected
+    disp('H0 is not rejected: no statistically significant difference found.');
+end
 disp(ResultsTable);
